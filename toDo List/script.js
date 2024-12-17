a1337bf2e33a1f446cdc23c5e822fe025efcf7b4
Bobbishly <ayomiderichard255@gmail.com>
2024-12-17 15:51:05 +0100

    Succesfully working

--- toDo List/script.js
+++ toDo List/script.js
@@ -2,12 +2,22 @@ const inputedValues = document.getElementById("list");
 const submitValues = document.getElementById("addText");
 const unOrderedList = document.getElementById("unOrdered");
 
+var addToTheList = function() {
+    const newList = document.createElement("li");
+    newList.appendChild(document.createTextNode(inputedValues.value));
+    unOrderedList.appendChild(newList);
+    inputedValues.value = "";
+}       
+
 submitValues.addEventListener('click', function() {
     // console.log("Hiiii");
     if (inputedValues.value.length > 0) {
-        const newList = document.createElement("li");
-        newList.appendChild(document.createTextNode(inputedValues.value));
-        unOrderedList.appendChild(newList);
-        inputedValues.value = "";
+        addToTheList();
+    }
+})
+
+inputedValues.addEventListener('keydown', function(event) {
+    if (inputedValues.value.length > 0 && event.key === "Enter") {
+        addToTheList();
     }
 })
