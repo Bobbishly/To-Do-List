83c1735ae864d03f1814d4fa2c1b6a3df0b40efd
Bobbishly <ayomiderichard255@gmail.com>
2024-12-17 15:33:03 +0100

    Adding file

--- toDo List/script.js
+++ toDo List/script.js
@@ -1,6 +1,7 @@
 var inputedValues = document.getElementById("list");
-
 var submitValues = document.getElementById("addText");
-
 var unOrderedList = document.getElementById("unOrdered");
 
+submitValues.addEventListener('click', function() {
+    console.log("Hiiii");
+})
